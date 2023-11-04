c140f7695727379f5e22d603bdf00cbff174c572
Reymon81 <ramon.torres.81@gmail.com>
2023-11-04 02:49:23 +0100

    RTC - Modelos

---
+++ src/main/java/com/springboot/app/item/models/Item.java
@@ -0,0 +1,17 @@
+package com.springboot.app.item.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Item {
+
+    private Product product;
+
+    private int amount;
+}
